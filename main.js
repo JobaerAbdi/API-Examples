7e86df1f33adbd65fab5555b7fdddb6c3a74c9bc
JobaerAbdi <jobaerrahman757@gmail.com>
2022-10-01 09:27:33 +0600

    Next push

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const loadData = () =>{
     fetch('https://api.kanye.rest/')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => kanyeUncle(data))
 }
-loadData()
+
+const kanyeUncle = users => {
+    const mainContainer = document.getElementById('input-data');
+    mainContainer.innerText = users.quote;
+}
+
+// const kanyeUncle = users => {
+//     const mainContainer = document.getElementById('input-data');
+//     const div = document.createElement('div');
+//     div.innerHTML = `
+//         <p>Kanye Information: ${users.quote}</p>
+//     `;
+//     mainContainer.appendChild(div);
+// }
